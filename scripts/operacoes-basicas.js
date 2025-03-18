b517dfdfae05de86563d833953cd8374824b039d
renatocn86 <96842858+renatocn86@users.noreply.github.com>
2025-03-18 08:55:29 -0300

    Operações básicas
    Criação do script operações básicas

---
+++ scripts/operacoes-basicas.js
@@ -0,0 +1,14 @@
+function OperacoesBasicas() {
+  var a = 35
+  var b = 66
+
+  var soma = a + b
+  var subtracao = a - b
+  var multiplicacao = a * b
+  var divisao = a / b
+
+  alert("Soma: " + soma + "\n" +
+    "Subtração: " + subtracao + "\n" +
+    "Multiplicação: " + multiplicacao + "\n" +
+    "Divisão: " + divisao)
+}
